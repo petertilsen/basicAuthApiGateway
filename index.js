8c4c17d72ef8bc285ece13a8c44596d59c892404
petertilsen <peter@uniplaces.com>
2017-05-24 11:27:10 +0200

    minor fix

--- index.js
+++ index.js
@@ -11,28 +11,29 @@ exports.handler = (event, context, callback, config) => {
   var user = userFromBasicAuthString(token);
 
   if (user.name === process.env.AUTH_USER && user.pass === process.env.AUTH_PASS) {
+    console.log("Authorized for name: " + user.name + " and methodArn: " + event.methodArn);
     context.succeed(generatePolicy(user.name, 'Allow', event.methodArn));
     return;
   }
-    
+  
   context.fail("Unauthorized");
 };
 
 userFromBasicAuthString = (header) => {
-  if (!header) {
-    console.log('no header provided');
-    return null;
-  }
-
   var match = credentialsRegExp.exec(header || '');
 
   if (!match) {
+    if (!header) {
+      console.log('no header provided');
+      return null;
+    }
     match = ['', header];
   }
 
   var userPass = userPassRegExp.exec(decodeBase64(match[1]));
 
   if (!userPass) {
+    console.log("No user password provided");
     return null;
   }
 
@@ -48,7 +49,7 @@ generatePolicy = (principalId, effect, resource) => {
   authResponse.principalId = principalId;
   if (effect && resource) {
     var policyDocument = {};
-    policyDocument.Version = '2017-04-13'; // default version
+    policyDocument.Version = '2012-10-17'; // default version
     policyDocument.Statement = [];
     var statementOne = {};
     statementOne.Action = 'execute-api:Invoke'; // default action
@@ -57,6 +58,6 @@ generatePolicy = (principalId, effect, resource) => {
     policyDocument.Statement[0] = statementOne;
     authResponse.policyDocument = policyDocument;
   }
-  
+  console.log(JSON.stringify(authResponse));
   return authResponse;
 }
